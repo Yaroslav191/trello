7508dfd8d53fd5b20cc2b85b6542ca12a66d3f8e
Yaroslav <dunadein_5@mail.ru>
2023-11-07 00:13:41 +0600

    fix

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,17 +1,23 @@
 'use client';
 
+import { useBoardStore } from '@/store/BoardStore';
 import React, { useEffect } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Board = () => {
-  useEffect(() => {}, []);
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, []);
 
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div></div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>Hello</h1>
+    //   <DragDropContext>
+    //     <Droppable droppableId="board" direction="horizontal" type="column">
+    //       {(provided) => <div></div>}
+    //     </Droppable>
+    //   </DragDropContext>
   );
 };
 
