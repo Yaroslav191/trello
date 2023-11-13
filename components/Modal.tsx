d67108871ac84a0f704e8887c9e34a49c28d1bde
Yaroslav <dunadein_5@mail.ru>
2023-11-13 23:33:10 +0600

    fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,144 +1,143 @@
-"use client";
+'use client';
 
-import { useState, Fragment } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { useModalStore } from "@/store/ModalStore";
-import { CheckBadgeIcon, PhotoIcon } from "@heroicons/react/20/solid";
+import { useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { useModalStore } from '@/store/ModalStore';
+import { CheckBadgeIcon, PhotoIcon } from '@heroicons/react/20/solid';
+import { storage } from '@/appwrite';
 
 function Modal() {
-   const [isOpen, closeModal] = useModalStore((state) => [
-      state.isOpen,
-      state.closeModal,
-   ]);
+  const [isOpen, closeModal] = useModalStore((state) => [state.isOpen, state.closeModal]);
 
-   const active = "bg-yellow-400 text-white";
+  const active = 'bg-yellow-400 text-white';
 
-   const [isOptionActive, setSIsOptionActive] = useState(false);
-   const [optionState, setOptionState] = useState(0);
+  const [isOptionActive, setSIsOptionActive] = useState(false);
+  const [optionState, setOptionState] = useState(0);
+  const [file, setFile] = useState('');
 
-   const onSubmit = (e: any) => {
-      e.preventDefault();
-      console.log("test");
-   };
+  const onSubmit = (e: any) => {
+    e.preventDefault();
 
-   const selectOption = (option: number) => {
-      setOptionState(option);
-   };
+    const promise = storage.createFile(process.env.NEXT_PUBLIC_BUCKET_ID!, '4', file);
 
-   return (
-      // Use the `Transition` component at the root level
-      <Transition appear show={isOpen} as={Fragment}>
-         <Dialog as="form" onClose={closeModal} className="relative z-10">
+    promise.then(
+      function (response) {
+        console.log(response); // Success
+      },
+      function (error) {
+        console.log(error); // Failure
+      },
+    );
+  };
+
+  const selectOption = (option: number) => {
+    setOptionState(option);
+  };
+
+  return (
+    // Use the `Transition` component at the root level
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="form" onClose={closeModal} className="relative z-10">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0">
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
-               as={Fragment}
-               enter="ease-out duration-300"
-               enterFrom="opacity-0"
-               enterTo="opacity-100"
-               leave="ease-in duration-200"
-               leaveFrom="opacity-100"
-               leaveTo="opacity-0"
-            >
-               <div className="fixed inset-0 bg-black bg-opacity-25" />
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 pb-2">
+                  Add a Task
+                </Dialog.Title>
+
+                <div className="mb-5">
+                  <input
+                    type="text"
+                    placeholder="Enter a task here..."
+                    className="w-full p-2 outline-none border rounded-lg"
+                  />
+                </div>
+
+                <div
+                  className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
+                    optionState === 1 ? active : ''
+                  }`}
+                  onClick={() => selectOption(1)}>
+                  <div>
+                    <div className="">Todo</div>
+                    <div>A new task to be completed</div>
+                  </div>
+                  <div>
+                    <CheckBadgeIcon className="w-7 h-7" />
+                  </div>
+                </div>
+
+                <div
+                  className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
+                    optionState === 2 ? active : ''
+                  }`}
+                  onClick={() => selectOption(2)}>
+                  <div>
+                    <div className="">In Progress</div>
+                    <div>A task that is currently being worked on</div>
+                  </div>
+                  <div>
+                    <CheckBadgeIcon className="w-7 h-7" />
+                  </div>
+                </div>
+
+                <div
+                  className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
+                    optionState === 3 ? active : ''
+                  }`}
+                  onClick={() => selectOption(3)}>
+                  <div>
+                    <div className="">Done</div>
+                    <div>A task that has been completed</div>
+                  </div>
+                  <div>
+                    <CheckBadgeIcon className="w-7 h-7" />
+                  </div>
+                </div>
+
+                <div className="mt-5 mb-3">
+                  <label
+                    htmlFor="upload"
+                    className="w-full p-5 flex justify-center items-center border rounded-lg cursor-pointer">
+                    <PhotoIcon className="w-5 h-5 mr-2" />
+                    Upload Image
+                  </label>
+                  <input
+                    type="file"
+                    id="upload"
+                    className="hidden"
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </div>
+                <button className="p-2 bg-gray-200 rounded-lg" type="submit" onClick={onSubmit}>
+                  Add Task
+                </button>
+              </Dialog.Panel>
             </Transition.Child>
-
-            <div className="fixed inset-0 overflow-auto">
-               <div className="flex min-h-full items-center justify-center p-4 text-center">
-                  <Transition.Child
-                     as={Fragment}
-                     enter="ease-out duration-300"
-                     enterFrom="opacity-0"
-                     enterTo="opacity-100"
-                     leave="ease-in duration-200"
-                     leaveFrom="opacity-100"
-                     leaveTo="opacity-0"
-                  >
-                     <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                        <Dialog.Title
-                           as="h3"
-                           className="text-lg font-medium leading-6 text-gray-900 pb-2"
-                        >
-                           Add a Task
-                        </Dialog.Title>
-
-                        <div className="mb-5">
-                           <input
-                              type="text"
-                              placeholder="Enter a task here..."
-                              className="w-full p-2 outline-none border rounded-lg"
-                           />
-                        </div>
-
-                        <div
-                           className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
-                              optionState === 1 ? active : ""
-                           }`}
-                           onClick={() => selectOption(1)}
-                        >
-                           <div>
-                              <div className="">Todo</div>
-                              <div>A new task to be completed</div>
-                           </div>
-                           <div>
-                              <CheckBadgeIcon className="w-7 h-7" />
-                           </div>
-                        </div>
-
-                        <div
-                           className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
-                              optionState === 2 ? active : ""
-                           }`}
-                           onClick={() => selectOption(2)}
-                        >
-                           <div>
-                              <div className="">In Progress</div>
-                              <div>
-                                 A task that is currently being worked on
-                              </div>
-                           </div>
-                           <div>
-                              <CheckBadgeIcon className="w-7 h-7" />
-                           </div>
-                        </div>
-
-                        <div
-                           className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
-                              optionState === 3 ? active : ""
-                           }`}
-                           onClick={() => selectOption(3)}
-                        >
-                           <div>
-                              <div className="">Done</div>
-                              <div>A task that has been completed</div>
-                           </div>
-                           <div>
-                              <CheckBadgeIcon className="w-7 h-7" />
-                           </div>
-                        </div>
-
-                        <div className="mt-5 mb-3">
-                           <label
-                              htmlFor="upload"
-                              className="w-full p-5 flex justify-center items-center border rounded-lg cursor-pointer"
-                           >
-                              <PhotoIcon className="w-5 h-5 mr-2" />
-                              Upload Image
-                           </label>
-                           <input type="file" id="upload" className="hidden" />
-                        </div>
-                        <button
-                           className="p-2 bg-gray-200 rounded-lg"
-                           type="submit"
-                           onClick={onSubmit}
-                        >
-                           Add Task
-                        </button>
-                     </Dialog.Panel>
-                  </Transition.Child>
-               </div>
-            </div>
-         </Dialog>
-      </Transition>
-   );
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
 }
 
 export default Modal;
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,56 +1,41 @@
-"use client";
+'use client';
 
 import {
-   DraggableProvidedDragHandleProps,
-   DraggableProvidedDraggableProps,
-} from "@hello-pangea/dnd";
-import { XCircleIcon } from "@heroicons/react/20/solid";
-import Image from "next/image";
-import React from "react";
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from '@hello-pangea/dnd';
+import { XCircleIcon } from '@heroicons/react/20/solid';
+import Image from 'next/image';
+import React from 'react';
 
 type Props = {
-   todo: Todo;
-   index: number;
-   id: TypedColumn;
-   innerRef: (element: HTMLElement | null) => void;
-   draggableProps: DraggableProvidedDraggableProps;
-   dragHandleProps: DraggableProvidedDragHandleProps;
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps;
 };
 
-function TodoCard({
-   todo,
-   index,
-   id,
-   innerRef,
-   draggableProps,
-   dragHandleProps,
-}: Props) {
-   console.log(todo);
+function TodoCard({ todo, index, id, innerRef, draggableProps, dragHandleProps }: Props) {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}>
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
 
-   return (
-      <div
-         className="bg-white rounded-md space-y-2 drop-shadow-md"
-         {...draggableProps}
-         {...dragHandleProps}
-         ref={innerRef}
-      >
-         <div className="flex justify-between items-center p-5">
-            <p>{todo.title}</p>
-
-            <button className="text-red-500 hover:text-red-600">
-               <XCircleIcon className="ml-5 h-8 w-8" />
-            </button>
-         </div>
-         {todo.image && (
-            <Image
-               src={todo.image}
-               width={500}
-               height={400}
-               alt="Picture of the author"
-            />
-         )}
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
       </div>
-   );
+      {todo.image && (
+        <Image src={todo.image} width={500} height={400} alt="Picture of the author" />
+      )}
+    </div>
+  );
 }
 
 export default TodoCard;
