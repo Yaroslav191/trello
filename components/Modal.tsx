17caf0e47b09676e095e08de7462ba2bd1e3fea8
Yaroslav <yaroslavv86@gmail.com>
2023-11-16 17:46:03 +0600

    fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,89 +1,103 @@
-'use client';
+"use client";
 
-import { useState, Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { useModalStore } from '@/store/ModalStore';
-import { CheckBadgeIcon, PhotoIcon } from '@heroicons/react/20/solid';
-import { databases, storage } from '@/appwrite';
-import { v4 as uuidv4 } from 'uuid';
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { useModalStore } from "@/store/ModalStore";
+import { CheckBadgeIcon, PhotoIcon } from "@heroicons/react/20/solid";
+import { databases, storage } from "@/appwrite";
+import { v4 as uuidv4 } from "uuid";
+import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
 
 function Modal() {
-  const [isOpen, closeModal] = useModalStore((state) => [state.isOpen, state.closeModal]);
+   const [isOpen, closeModal] = useModalStore((state) => [
+      state.isOpen,
+      state.closeModal,
+   ]);
 
-  const active = 'bg-yellow-400 text-white';
+   const active = "bg-yellow-400 text-white";
 
-  const [isOptionActive, setSIsOptionActive] = useState(false);
-  const [optionState, setOptionState] = useState(0);
-  const [file, setFile] = useState(null);
-  const [title, setTitle] = useState('');
+   const [isOptionActive, setSIsOptionActive] = useState(false);
+   const [optionState, setOptionState] = useState(0);
+   const [file, setFile] = useState(null);
+   const [title, setTitle] = useState("");
 
-  const id = uuidv4();
+   const id = uuidv4();
 
-  const statuses = ['todo', 'inprogress', 'done'];
+   const statuses = ["todo", "inprogress", "done"];
 
-  const onSubmit = (e: any) => {
-    e.preventDefault();
+   const onSubmit = (e: any) => {
+      e.preventDefault();
 
-    const saveForm = async () => {
-      const fileResponse = await storage.createFile(process.env.NEXT_PUBLIC_BUCKET_ID!, id, file);
+      const saveForm = async () => {
+         const fileResponse = await storage.createFile(
+            process.env.NEXT_PUBLIC_BUCKET_ID!,
+            id,
+            file
+         );
 
-      const documentResponse = await databases.createDocument(
-        process.env.NEXT_PUBLIC_DATABASE_ID!,
-        '6547bad096ffa6018276',
-        uuidv4(),
-        { title: title, status: statuses[optionState], image: id },
-      );
+         const documentResponse = await databases.createDocument(
+            process.env.NEXT_PUBLIC_DATABASE_ID!,
+            "6547bad096ffa6018276",
+            uuidv4(),
+            { title: title, status: statuses[optionState], image: id }
+         );
 
-      console.log(fileResponse, documentResponse);
-    };
+         console.log(fileResponse, documentResponse);
+      };
 
-    saveForm();
-  };
+      saveForm();
+   };
 
-  const selectOption = (option: number) => {
-    setOptionState(option);
-  };
+   const selectOption = (option: number) => {
+      setOptionState(option);
+   };
 
-  return (
-    // Use the `Transition` component at the root level
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="form" onClose={closeModal} className="relative z-10">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0">
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+   return (
+      // Use the `Transition` component at the root level
+      <Transition appear show={isOpen} as={Fragment}>
+         <Dialog as="form" onClose={closeModal} className="relative z-10">
             <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0">
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 pb-2">
-                  Add a Task
-                </Dialog.Title>
+               as={Fragment}
+               enter="ease-out duration-300"
+               enterFrom="opacity-0"
+               enterTo="opacity-100"
+               leave="ease-in duration-200"
+               leaveFrom="opacity-100"
+               leaveTo="opacity-0"
+            >
+               <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
 
-                <div className="mb-5">
-                  <input
-                    type="text"
-                    placeholder="Enter a task here..."
-                    className="w-full p-2 outline-none border rounded-lg"
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                </div>
+            <div className="fixed inset-0 overflow-auto">
+               <div className="flex min-h-full items-center justify-center p-4 text-center">
+                  <Transition.Child
+                     as={Fragment}
+                     enter="ease-out duration-300"
+                     enterFrom="opacity-0"
+                     enterTo="opacity-100"
+                     leave="ease-in duration-200"
+                     leaveFrom="opacity-100"
+                     leaveTo="opacity-0"
+                  >
+                     <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        <Dialog.Title
+                           as="h3"
+                           className="text-lg font-medium leading-6 text-gray-900 pb-2"
+                        >
+                           Add a Task
+                        </Dialog.Title>
 
-                <div
+                        <div className="mb-5">
+                           <input
+                              type="text"
+                              placeholder="Enter a task here..."
+                              className="w-full p-2 outline-none border rounded-lg"
+                              onChange={(e) => setTitle(e.target.value)}
+                           />
+                        </div>
+                        <TaskTypeRadioGroup />
+
+                        {/* <div
                   className={`flex justify-between items-center cursor-pointer mb-3 shadow-md p-3 rounded-lg ${
                     optionState === 1 ? active : ''
                   }`}
@@ -141,14 +155,14 @@ function Modal() {
                 </div>
                 <button className="p-2 bg-gray-200 rounded-lg" type="submit" onClick={onSubmit}>
                   Add Task
-                </button>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
+                </button> */}
+                     </Dialog.Panel>
+                  </Transition.Child>
+               </div>
+            </div>
+         </Dialog>
+      </Transition>
+   );
 }
 
 export default Modal;
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,41 +1,59 @@
-'use client';
+"use client";
 
+import { useBoardStore } from "@/store/BoardStore";
 import {
-  DraggableProvidedDragHandleProps,
-  DraggableProvidedDraggableProps,
-} from '@hello-pangea/dnd';
-import { XCircleIcon } from '@heroicons/react/20/solid';
-import Image from 'next/image';
-import React from 'react';
+   DraggableProvidedDragHandleProps,
+   DraggableProvidedDraggableProps,
+} from "@hello-pangea/dnd";
+import { XCircleIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import React from "react";
 
 type Props = {
-  todo: Todo;
-  index: number;
-  id: TypedColumn;
-  innerRef: (element: HTMLElement | null) => void;
-  draggableProps: DraggableProvidedDraggableProps;
-  dragHandleProps: DraggableProvidedDragHandleProps;
+   todo: Todo;
+   index: number;
+   id: TypedColumn;
+   innerRef: (element: HTMLElement | null) => void;
+   draggableProps: DraggableProvidedDraggableProps;
+   dragHandleProps: DraggableProvidedDragHandleProps;
 };
 
-function TodoCard({ todo, index, id, innerRef, draggableProps, dragHandleProps }: Props) {
-  return (
-    <div
-      className="bg-white rounded-md space-y-2 drop-shadow-md"
-      {...draggableProps}
-      {...dragHandleProps}
-      ref={innerRef}>
-      <div className="flex justify-between items-center p-5">
-        <p>{todo.title}</p>
+function TodoCard({
+   todo,
+   index,
+   id,
+   innerRef,
+   draggableProps,
+   dragHandleProps,
+}: Props) {
+   const deleteTask = useBoardStore((state) => state.deleteTask);
+   return (
+      <div
+         className="bg-white rounded-md space-y-2 drop-shadow-md"
+         {...draggableProps}
+         {...dragHandleProps}
+         ref={innerRef}
+      >
+         <div className="flex justify-between items-center p-5">
+            <p>{todo.title}</p>
 
-        <button className="text-red-500 hover:text-red-600">
-          <XCircleIcon className="ml-5 h-8 w-8" />
-        </button>
+            <button
+               onClick={() => deleteTask(index, todo, id)}
+               className="text-red-500 hover:text-red-600"
+            >
+               <XCircleIcon className="ml-5 h-8 w-8" />
+            </button>
+         </div>
+         {todo.image && (
+            <Image
+               src={todo.image}
+               width={500}
+               height={400}
+               alt="Picture of the author"
+            />
+         )}
       </div>
-      {todo.image && (
-        <Image src={todo.image} width={500} height={400} alt="Picture of the author" />
-      )}
-    </div>
-  );
+   );
 }
 
 export default TodoCard;
